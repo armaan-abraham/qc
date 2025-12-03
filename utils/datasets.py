0914c6412a8ed772e86d1e8ad87ace645196b11e
Armaan Abraham <armaanabraham@g.ucla.edu>
2025-12-03 11:18:08 -0800

    dataset and buffer

--- utils/datasets.py
+++ utils/datasets.py
@@ -4,6 +4,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from flax.core.frozen_dict import FrozenDict
+from typing import Sequence
 
 
 def get_size(data):
@@ -17,22 +18,35 @@ def get_pair_rel_utils(utils_to_terminals, times_to_terminals, discount: float):
     batch_size, seq_len = utils_to_terminals.shape
     rel_times = times_to_terminals[:, :, None] - times_to_terminals[:, None, :]
     assert rel_times.shape == (batch_size, seq_len, seq_len)
-    print("Relative times:")
-    print(rel_times)
     later_coeffs = discount ** jnp.abs(rel_times)
-    print("Later coefficients:")
-    print(later_coeffs)
     util_diffs = jnp.where(
         rel_times > 0,
         utils_to_terminals[:, :, None] - later_coeffs * utils_to_terminals[:, None, :],
         0.0,
     )
     valid_mask = rel_times > 0
-    print("Utility differences:")
-    print(util_diffs)
-    print("Valid mask:")
-    print(valid_mask.astype(np.int32))
     return util_diffs, valid_mask
+
+def get_utils_and_times_to_terminals(dataset, discount: float):
+    # Compute discounted sum of rewards to the next terminal. These are NOT
+    # valid unless used to compute utils between transitions on a relative
+    # basis, because these are relative to terminals, not episode
+    # completions.
+    rewards = dataset['rewards']
+    terminals = dataset['terminals']
+
+    utils_to_terminals = np.zeros_like(rewards)
+    util_to_terminal = 0.0
+    times_to_terminals = np.zeros_like(rewards)
+    next_terminal_idx = len(rewards) - 1
+    for t_idx in range(len(rewards) - 1, -1, -1):
+        if terminals[t_idx] > 0:
+            next_terminal_idx = t_idx
+        util_to_terminal = rewards[t_idx] + discount * util_to_terminal * (1 - terminals[t_idx])
+        utils_to_terminals[t_idx] = util_to_terminal
+        times_to_terminals[t_idx] = next_terminal_idx - t_idx
+    return utils_to_terminals, times_to_terminals
+
 
 class Dataset(FrozenDict):
     """Dataset class."""
@@ -54,64 +68,36 @@ class Dataset(FrozenDict):
         # Set the final transition to a terminal
         data['terminals'][-1] = 1.0
 
-        print("Terminals after adjustment:")
-        print(data['terminals'])
-
-
-        # Compute discounted sum of rewards to the next terminal. These are NOT
-        # valid unless used to compute utils between transitions on a relative
-        # basis, because these are relative to terminals, not episode
-        # completions.
-        rewards = data['rewards']
-        utils_to_terminals = np.zeros_like(rewards)
-        util_to_terminal = 0.0
-        times_to_terminal = np.zeros_like(rewards)
-        next_terminal_idx = len(rewards) - 1
-        for t_idx in range(len(rewards) - 1, -1, -1):
-            if data['terminals'][t_idx] > 0:
-                next_terminal_idx = t_idx
-            util_to_terminal = rewards[t_idx] + discount * util_to_terminal * (1 - data['terminals'][t_idx])
-            utils_to_terminals[t_idx] = util_to_terminal
-            times_to_terminal[t_idx] = next_terminal_idx - t_idx
+        utils_to_terminals, times_to_terminals = get_utils_and_times_to_terminals(data, discount)
         data['utils_to_terminals'] = utils_to_terminals
-        print("Utils to terminals:")
-        print(data['utils_to_terminals'])
-        data['times_to_terminals'] = times_to_terminal
-        print("Times to terminals:")
-        print(data['times_to_terminals'])
+        data['times_to_terminals'] = times_to_terminals
         if freeze:
             jax.tree_util.tree_map(lambda arr: arr.setflags(write=False), data)
         return cls(data)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.size = get_size(self._dict)
-
+    
+    def init_term_locs(self):
         # Store terminal locations for sampling within episodes
         self.terminal_locs = np.nonzero(self['terminals'] > 0)[0]
-        print("Terminal locations:")
-        print(self.terminal_locs)
 
         # Store trajectory start locations
         self.start_locs = np.concatenate(([0], self.terminal_locs[:-1] + 1))
-        print("Start locations:")
-        print(self.start_locs)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.size = get_size(self._dict)
+        self.init_term_locs()
 
 
-    def sample_in_episodes(self, batch_size: int, sequence_length: int):
+
+    def sample_in_trajectories(self, batch_size: int, sequence_length: int):
         """Sample transitions and return a batch of shape [batch_size, sequence_length, ...], 
         where all transitions within a sequence are from the same episode.
         Episodes are chosen proportionally to their length."""
 
         episode_lens = self.terminal_locs - self.start_locs + 1
-        print("Episode lengths:")
-        print(episode_lens)
         episode_probs = episode_lens / episode_lens.sum()
-        print("Episode probabilities:")
-        print(episode_probs)
         sampled_episodes = np.random.choice(len(episode_lens), size=batch_size, p=episode_probs)
-        print("Sampled episodes:")
-        print(sampled_episodes)
 
         # Get start locations and lengths for sampled episodes
         starts = self.start_locs[sampled_episodes]
@@ -122,94 +108,88 @@ class Dataset(FrozenDict):
 
         # Compute transition indices
         transition_idxs = starts[:, None] + offsets
-        print("Transition indices:")
-        print(transition_idxs)
 
         # Index into dataset
         return jax.tree_util.tree_map(lambda arr: arr[transition_idxs], self._dict)
 
 
-# class ReplayBuffer(Dataset):
-#     """Replay buffer class.
+class ReplayBuffer(Dataset):
+    """Replay buffer class.
 
-#     This class extends Dataset to support adding transitions.
-#     """
+    This class extends Dataset to support adding episodes of transitions.
+    """
 
-#     @classmethod
-#     def create(cls, transition, size):
-#         """Create a replay buffer from the example transition.
+    @classmethod
+    def create_from_initial_dataset(cls, init_dataset, size):
+        """Create a replay buffer from the initial dataset.
 
-#         Args:
-#             transition: Example transition (dict).
-#             size: Size of the replay buffer.
-#         """
+        Args:
+            init_dataset: Initial dataset.
+            size: Size of the replay buffer.
+        """
 
-#         def create_buffer(example):
-#             example = np.array(example)
-#             return np.zeros((size, *example.shape), dtype=example.dtype)
+        def create_buffer(init_buffer):
+            buffer = np.zeros((size, *init_buffer.shape[1:]), dtype=init_buffer.dtype)
+            buffer[: len(init_buffer)] = init_buffer
+            return buffer
 
-#         buffer_dict = jax.tree_util.tree_map(create_buffer, transition)
-#         return cls(buffer_dict)
+        buffer_dict = jax.tree_util.tree_map(create_buffer, init_dataset)
+        buffer = cls(buffer_dict)
+        buffer.size = get_size(init_dataset)
+        buffer.pointer = buffer.size % size
+        return buffer
 
-#     @classmethod
-#     def create_from_initial_dataset(cls, init_dataset, size):
-#         """Create a replay buffer from the initial dataset.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-#         Args:
-#             init_dataset: Initial dataset.
-#             size: Size of the replay buffer.
-#         """
+        self.max_size = get_size(self._dict)
+        self.size = 0
+        self.pointer = 0
 
-#         def create_buffer(init_buffer):
-#             buffer = np.zeros((size, *init_buffer.shape[1:]), dtype=init_buffer.dtype)
-#             buffer[: len(init_buffer)] = init_buffer
-#             return buffer
+    def add_trajectory(self, trajectory: Sequence[dict], discount: float):
+        """Add a trajectory of transitions to the replay buffer."""
+        num_transitions = len(trajectory)
 
-#         buffer_dict = jax.tree_util.tree_map(create_buffer, init_dataset)
-#         dataset = cls(buffer_dict)
-#         dataset.size = get_size(init_dataset)
-#         dataset.pointer = dataset.size % size
-#         return dataset
+        # Convert trajectory to dict of arrays
+        trajectory = jax.tree_util.tree_map(lambda *xs: np.stack(xs), *trajectory)
+        assert isinstance(trajectory, dict)
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+        terminal_locs = np.nonzero(trajectory['terminals'] > 0)[0]
+        assert terminal_locs.size == 1 and terminal_locs[0] == num_transitions - 1
+        assert np.all(trajectory['terminals'][trajectory['masks'] == 0] == 1)
 
-#         self.max_size = get_size(self._dict)
-#         self.size = 0
-#         self.pointer = 0
-#         self.last_transition_terminal = True
+        # We populate the times and utils to terminals based on the entire
+        # trajectory, despite the possibility of dividing the trajectory when
+        # looping around the buffer. This is fine because these values are only
+        # used on a relative basis.
+        utils_to_terminals, times_to_terminals = get_utils_and_times_to_terminals(trajectory, discount)
+        trajectory['utils_to_terminals'] = utils_to_terminals
+        trajectory['times_to_terminals'] = times_to_terminals
 
-#     def add_transition(self, transition):
-#         """Add a transition to the replay buffer."""
-#         # When adding new transitions, we will be overwriting old data and thus
-#         # ruining the episode structure. To deal with this, we set the last
-#         # added transition's terminal to 1 always, and if the true terminal was
-#         # 0, set it back to 0 once we add the next transition. When sampling, we
-#         # assume the episode that the pointer overlaps with is invalid.
+        for t_idx in range(num_transitions):
+            transition = jax.tree_util.tree_map(lambda arr: arr[t_idx], trajectory)
 
-#         self.last_transition_terminal = transition['terminals']
-#         assert isinstance(self.last_transition_terminal, float) or isinstance(self.last_transition_terminal, int)
+            def set_idx(buffer, new_element):
+                buffer[self.pointer] = new_element
 
-#         transition['terminals'] = max(1 - transition['masks'], transition['terminals'])
+            jax.tree_util.tree_map(set_idx, self._dict, transition)
 
-#         def set_idx(buffer, new_element):
-#             if not self.last_transition_terminal:
-#                 prev_pointer = (self.pointer - 1) % self.max_size
-#                 buffer[prev_pointer]['terminals'] = 0.0
-#             self.last_transition_terminal = new_element['terminals']
-#             new_element['terminals'] = 1.0
-#             buffer[self.pointer] = new_element
+            # If looping around, we set this terminal to 1
+            if self.pointer == self.max_size - 1:
+                self._dict['terminals'][self.pointer] = 1.0
 
-#         jax.tree_util.tree_map(set_idx, self._dict, transition)
-#         self.pointer = (self.pointer + 1) % self.max_size
-#         self.size = max(self.pointer, self.size)
+            self.pointer = (self.pointer + 1) % self.max_size
+            self.size = max(self.pointer, self.size)
 
-#     def clear(self):
-#         """Clear the replay buffer."""
-#         self.size = self.pointer = 0
+        self.init_term_locs()
+
+    def clear(self):
+        """Clear the replay buffer."""
+        self.size = self.pointer = 0
 
 if __name__ == "__main__":
     np.random.seed(3)
+    discount = 0.9
     # Simple test
     data = {
         'observations': np.arange(6),
@@ -229,12 +209,41 @@ if __name__ == "__main__":
         print(v)
     print()
 
-    dataset = Dataset.create(discount=0.9, **data)
-    batch = dataset.sample_in_episodes(batch_size=3, sequence_length=4)
+    dataset = Dataset.create(discount=discount, **data)
+    batch = dataset.sample_in_trajectories(batch_size=3, sequence_length=4)
     print("Sampled batch:")
     print("Utils to terminals:")
     print(batch['utils_to_terminals'])
     print("Times to terminals:")
     print(batch['times_to_terminals'])
 
-    pair_rel_utils, valid_mask = get_pair_rel_utils(batch["utils_to_terminals"], batch["times_to_terminals"], discount=0.9)
+
+    buffer = ReplayBuffer.create_from_initial_dataset(dict(dataset), size=7)
+    print("Initial buffer:")
+    for k, v in buffer._dict.items():
+        print(f"{k}:")
+        print(v)
+    
+    num_transitions = 3
+    trajectory = [
+        {
+            'observations': np.array(i + 6),
+            'next_observations': np.array(i + 7),
+            'actions': np.array((i + 6) * 2),
+            'rewards': (i + 6) * 0.5 + 1,
+            'masks': 1 if i < num_transitions else 0,
+            'terminals': 1 if i == num_transitions - 1 else 0,
+        }
+        for i in range(num_transitions)
+    ]
+    print("Adding trajectory:")
+    print(trajectory)
+    buffer.add_trajectory(trajectory, discount=discount)
+
+    print("Buffer after adding trajectory:")
+    for k, v in buffer._dict.items():
+        print(f"{k}:")
+        print(v)
+
+
+    # pair_rel_utils, valid_mask = get_pair_rel_utils(batch["utils_to_terminals"], batch["times_to_terminals"], discount=0.9)
